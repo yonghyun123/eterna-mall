41b10e283319a5f884b6e9b57ec4819c998e8ed6
00js00 <js000032@red.ujaen.es>
2018-11-12 11:55:20 +0900

    유효성 검사 전

--- src/main/java/kr/or/kosta/eterna/user/controller/UserUpdateController.java
+++ src/main/java/kr/or/kosta/eterna/user/controller/UserUpdateController.java
@@ -13,7 +13,6 @@ import org.json.simple.JSONObject;
 import kr.or.kosta.eterna.common.controller.Controller;
 import kr.or.kosta.eterna.common.controller.ModelAndView;
 import kr.or.kosta.eterna.common.factory.XMLObjectFactory;
-import kr.or.kosta.eterna.coupon.domain.Coupon;
 import kr.or.kosta.eterna.user.domain.User;
 import kr.or.kosta.eterna.user.service.UserService;
 import kr.or.kosta.eterna.user.service.UserServiceImpl;
@@ -37,10 +36,14 @@ public class UserUpdateController implements Controller {
 		userService = (UserService) factory.getBean(UserServiceImpl.class);
 		String loginId = null;
 		String inputPW = null;
+		String userEmail = null;
+		String confirmNewPW = null;
+		String newAddress = null;
+		
 		loginId = (String) request.getAttribute("loginId");
 		if (request.getParameter("inputPW") != null) {
 			inputPW = request.getParameter("inputPW");
-		}
+		
 		System.out.println("loginId : " + loginId);
 		System.out.println("inputPW : " + inputPW);
 		User userInfo = null;
@@ -54,9 +57,6 @@ public class UserUpdateController implements Controller {
 		String zipcode = token[0];
 		String streetAddress = token[1];
 		String detailAddress = token[2];
-		System.out.println("address 0 : "+token[0]);
-		System.out.println("address 1 : " + token[1]);
-		System.out.println("address 2 : " + token[2]);
 		JSONArray jsonArray = new JSONArray();
 
 			JSONObject jsonObject = new JSONObject();
@@ -78,7 +78,26 @@ public class UserUpdateController implements Controller {
 		}
 		System.out.println(jsonArray.toJSONString());
 		out.println(jsonArray.toJSONString());
-
+		} else if ((request.getParameter("userEmail") != null)||(request.getParameter("confirmNewPW") != null)||(request.getParameter("newAddress") != null)) {
+			userEmail = request.getParameter("userEmail");
+			confirmNewPW = request.getParameter("confirmNewPW");
+			newAddress = request.getParameter("newAddress");
+			System.out.println("userEmail : " + userEmail);
+			System.out.println("confirmNewPW : " + confirmNewPW);
+			System.out.println("newAddress : " + newAddress);
+			User user = new User();
+			user.setUserId(loginId);
+			user.setUserPasswd(confirmNewPW);
+			user.setUserEmail(userEmail);
+			user.setUserAddress(newAddress);
+			try {
+				System.out.println(user);
+				userService.update(user);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 
